d6ac010110cb86392ee3956a54cc7d1ba485e7b7
MaestroYao <109035118+MaestroYao@users.noreply.github.com>
2023-12-14 20:39:44 +0000

    Update 'db_utils.py'

--- db_utils.py
+++ db_utils.py
@@ -1,9 +1,33 @@
-
 import yaml
+import pandas as pd
+from pandas import DataFrame
+from sqlalchemy import create_engine
+  
 
-class RDSDatabaseConnector:
-    
-    def load_creds():
-        with open('.gitignore/credentials.yml', 'r') as file:
+print("hELLOW")
+def load_creds():
+        with open('/workspaces/exploratory-data-analysis---customer-loans-in-finance632/.gitignore/credentials.yaml', 'r') as file:
             credentials = yaml.safe_load(file)
         return credentials
+credentials = load_creds()
+print(credentials)
+
+""""
+
+class RDSDatabaseConnector():
+
+    def engine_creation(self):
+        RDSDatabaseConnector.load_creds()
+        engine = create_engine(self.credentials)
+
+        conn = engine.connect('loan_payments') 
+        sql_query = pd.read_sql_query ('''
+                                       SELECT *
+                                       FROM {table}
+                                       ''', conn)
+        df = pd.DataFrame(sql_query)
+        return df
+        
+    def __init__(self, credentials):
+        self._credentials = credentials
+"""
